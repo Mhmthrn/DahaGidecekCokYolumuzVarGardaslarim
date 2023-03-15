c4ac3c02b34beadc7ed06bba00932228efb15400
“Mehmet <70447221+harunapak@users.noreply.github.com>
2023-03-15 21:41:27 +0100

    mehmet 15.03 v1

---
+++ src/test/java/tests/C01_.java
@@ -0,0 +1,36 @@
+package tests;
+
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C01_  {
+
+    @Test
+    public void WebdriverUniTest(){
+
+        Driver.getDriver().get(ConfigReader.getProperty(""));
+
+          /*
+        1. “” sayfasina gidin
+        2. “Our Products” butonuna basin
+        3. “Cameras product”i tiklayin
+        4. Popup mesajini yazdirin
+        5. “close” butonuna basin
+        6. "WebdriverUniversity.com (IFrame)" linkini tiklayin
+        7. "http://webdriveruniversity.com/index.html" adresine gittigini test edin
+     */
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
